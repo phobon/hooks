76f422467ece42eebb8909337fc5b92ffdf66afa
Ben McCormick <exponen7@gmail.com>
2019-09-26 13:23:48 +0800

    Fixing up tests

--- src/useCachedState.js
+++ src/useCachedState.js
@@ -1,36 +1,29 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState } from 'react';
 
 const useCachedState = (key, initialValue = null) => {
-  const [state, setState] = useState(window.localStorage.getItem(key));
+  const [state, setState] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
 
-  // We need to get creative about how we set the initial state here.
-  // If there's no localStorage item and the initialValue is not null, set it.
-  // Pass in the key as a dep so this doesn't get called again til the key changes.
-  useEffect(() => {
-    if (state == null && initialValue) {
-      // Create a new state object and then stringify it here because we need to ensure
-      // that localState can handle it
-      const newState = {};
-      newState[key] = initialValue;
-      const storageState = JSON.stringify(newState);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  });
 
-      window.localStorage.setItem(key, storageState);
-      setState(storageState);
-    };
-  }, [key]);
+  const setCachedState = value => {
+    try {
+      const newState = value instanceof Function ? value(storedValue) : value;
+      setState(newState);
 
-  const setCachedState = useCallback((value) => {
-    const newState = JSON.parse(state);
-    newState[key] = value;
-    const storageState = JSON.stringify(newState);
+      window.localStorage.setItem(key, JSON.stringify(newState));
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-    setState(storageState);
-    window.localStorage.setItem(key, storageState);
-  }, [key, state]);
-
-  const newState = JSON.parse(state);
-
-  return [newState && newState[key], setCachedState];
+  return [state, setCachedState];
 };
 
 export default useCachedState;
